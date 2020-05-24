1684c39fd4e0c7a1ec8c7b2791187f23c8e4f950
Tom Wollnik <tom.wollnik@student.hpi.de>
2020-05-24 13:13:30 +0000

    add comments

--- builder.sh
+++ builder.sh
@@ -1,37 +1,52 @@
 #!/bin/bash
+# takes exactly one argument which is the name of the folder 
+# inside the current dir in which the .c files are
 
 OUT_DIR=.pex
+
 rm -rf $OUT_DIR
 mkdir -p $OUT_DIR
-
-# TODO: dont hard-code input file names
 cd $OUT_DIR 
+
+# create IR for every c file in the given directory
 clang -emit-llvm -S ../$1/*.c
 
+# create a tar archive with the .ll files
 tar -cvf prog.tar *
 
+# The script that will later be bundled with the tar archive
 LOADER_SCRIPT=\
 "#!/bin/bash
-
 mkdir -p .pex
-tail -n+17 \$0 | tar -x -C .pex
+
+# Find the tar archive inside the script and extract it.
+# In the option -n+XX , XX indicates the line in which
+# the tar archive starts.
+tail -n+21 \$0 | tar -x -C .pex
 
 cd .pex
+
+# compile the program
 clang -c *.ll 
 clang *.o 
 
-#rm -r .pex *.o
-
+# execute the program
 ./a.out
 
 exit 0
+
+# after this the archive will be injected
 #__ARCHIVE__BELOW__
 "
 
-# save portable executable
 cd ..
+
+# merge loader script and tar archive to portable executable
 echo "$LOADER_SCRIPT" > program.pex
 cat "$OUT_DIR"/prog.tar >> program.pex
+
+# make loader script executable
 chmod a+x program.pex
 
+# clean up
 rm -r $OUT_DIR
