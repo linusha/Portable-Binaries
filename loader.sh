3c6fe751903cbc564b153c4629059d8badf8e853
Tom Wollnik <tom.wollnik@student.hpi.de>
2020-06-01 16:18:49 +0000

    support adding own object files

--- loader.sh
+++ loader.sh
@@ -2,6 +2,7 @@
 # TODO: Add a comment that explains what this is.
 # TODO: Add licensing comment
 
+set -e
 
 ########## HELPERS ##########
 
@@ -14,17 +15,21 @@ function print_usage {
 	echo "    -t NAME Extract tar archive to directory NAME"
 	echo "    -T Extract tar archive to directory tar"
 	echo "    -r Force recompile"
+	echo "    -a NAME Add the object files in NAME to the tar archive"
+	echo "    -u NAME Use object files from NAME dir in tar archive"
 	echo "    -h Display this help message"
 	echo
 }
 
 ##### ARGUMENT PARSING  #####
 
-while getopts 't:Trh' flag; do
+while getopts 't:Trha:u:' flag; do
   case "${flag}" in
     t) TARDIR="${OPTARG}";;
     T) TARDIR="tar";;
     r) FORCE_RECOMPILE=true;;
+    a) ADD_OBJ_DIR="${OPTARG}";;
+    u) OBJ_DIR="${OPTARG}";;
     h) print_usage
        exit 1;;
   esac
@@ -43,7 +48,7 @@ TAR_START_POSITION=$(( $( grep -na '^#__ARCHIVE__BELOW__' $0 | grep -o '^[0-9]*'
 tail -n+$TAR_START_POSITION $0 | tar -x -C $OUT_DIR
 
 # If -t flag was set extract tar archive to $TARDIR and exit
-if [ $TARDIR ]; then
+if [[ $TARDIR ]]; then
 	log "extracting tar from $0 to $TARDIR"
 	rm -rf $TARDIR
 	mkdir -p $TARDIR
@@ -52,14 +57,25 @@ if [ $TARDIR ]; then
 	exit 0
 fi
 
-cd $OUT_DIR
-
-# create sub directory for current architecture and change into it
+# determine current cpu architecture
 ARCH=$( lscpu | head -n 1 | sed 's/Architecture:[[:space:]]*//g' )
 
+# If -a flag was set, add new object files
+if [[ $ADD_OBJ_DIR ]]; then
+	cp -r $ADD_OBJ_DIR $OUT_DIR
+	ARCH=$ADD_OBJ_DIR
+fi
+
+# If -u flag was set, use given folder name
+if [[ $OBJ_DIR ]]; then
+	log "using binaries in $OBJ_DIR"
+	ARCH=$OBJ_DIR
+fi
+
+cd $OUT_DIR
+
 # case 1: program is already compiled for the current architecture
-# TODO: Support manually inserting .o files
-if [[ -d $ARCH && ! $FORCE_RECOMPILE ]]; then
+if [[ -e $ARCH/a.out && ! $FORCE_RECOMPILE ]]; then
 	log "executing existing binary for $ARCH"
 	./$ARCH/a.out
 	log "done"
@@ -67,18 +83,25 @@ if [[ -d $ARCH && ! $FORCE_RECOMPILE ]]; then
 fi
 
 # case 2: program needs to be compiled from IR
-if [[ $FORCE_RECOMPILE ]]; then
-	log "forcing recompilation"
-else
-	log "no precompiled binary for $ARCH detected"
+if [[ $FORCE_RECOMPILE && $OBJ_DIR ]]; then
+	log "forcing recompilation from object files"
+	rm -f $ARCH/a.out
+elif [[ $FORCE_RECOMPILE && ! $OBJ_DIR ]]; then
+	log "forcing recompilation from IR"
+	rm -rf $ARCH
 fi
 
-rm -rf $ARCH
-mkdir $ARCH
+# create sub directory for current architecture and change into it
+mkdir -p $ARCH
 cd $ARCH
 
-log "compiling program from IR"
-clang -c ../*.ll 
+if [[ -z $ADD_OBJ_DIR && -z $OBJ_DIR ]]; then
+	# object files are not provided already
+	log "generating object files from IR"
+	clang -c ../*.ll 
+fi
+
+log "generating executable from object files"
 clang *.o 
 
 # TODO execute program in its original context (aka in BASE_DIR)
