595a7c2782b142ffe539a5fb3398b5db4c5ae80a
Linus Hagemann <linus.hagemann@posteo.net>
2020-07-08 20:42:52 +0200

    clean up loader code

--- loader.sh
+++ loader.sh
@@ -6,74 +6,80 @@
 set -e
 shopt -s globstar
 
-########## HELPERS ##########
-
 function log {
-	if [[ -n $PEX_VERBOSE ]]; then
-		echo \[PEX\] $1
+	# TODO: extract into separate file
+	# Logging function for PEX.
+    # Activate output by setting the environment
+    # variable PEX_VERBOSE when calling pex.
+    # Parameters:
+    # 1 - string to log
+    # Return Value:
+    # none
+	if [[ -n "$PEX_VERBOSE" ]]; then
+		echo \[PEX\] "$1"
 	fi
 }
 
-######### LOADER ############
-
-BASE_DIR=$PWD
-OUT_DIR=$(mktemp -d)
+BASEDIR="$PWD"
+TEMPDIR=$( mktemp -d )
 
 # Find the number of the line beginning with #__ARCHIVE__BELOW__ with grep.
 # Add one to account for newline.
-TAR_START_POSITION=$(( $( grep -na '^#__ARCHIVE__BELOW__' $0 | grep -o '^[0-9]*' ) + 1 ))
+TAR_START_POSITION=$(( $( grep -na '^#__ARCHIVE__BELOW__' "$0" | grep -o '^[0-9]*' ) + 1 ))
 
 # Extract the tar archive.
-tail -n+$TAR_START_POSITION $0 | tar -x -C $OUT_DIR
+tail -n+"$TAR_START_POSITION" "$0" | tar -x -C "$TEMPDIR"
 
-# determine current cpu architecture
-ARCH=$(clang -dumpmachine)
-
-# if the env variable PEX_USE_TAG is set, use corresponding bundle
-if [[ -n $PEX_USE_TAG ]]; then
-	log "pex tag is set to $PEX_USE_TAG"
-	ARCH=$PEX_USE_TAG
+if [[ -n "$PEX_USE_TAG" ]]; then
+	# if the env variable PEX_USE_TAG is set, use corresponding bundle
+	BUNDLE_NAME="$PEX_USE_TAG"
+else
+	# determine current cpu architecture
+	BUNDLE_NAME=$( clang -dumpmachine )
 fi
+log "pex tag is set to $BUNDLE_NAME"
 
-cd $OUT_DIR
+BUNDLE_PATH="$TEMPDIR"/"$BUNDLE_NAME"
 
-# programs needs to be compiled from IR
-if [[ ! -e $ARCH/a.out ]]; then
+# program needs to be compiled from IR
+if [[ ! -e "$BUNDLE_PATH"/a.out ]]; then
 	# create sub directory for current architecture and change into it
-	mkdir -p $ARCH
-	cd $ARCH
+	mkdir -p "$BUNDLE_PATH"
+	cd "$BUNDLE_PATH"
 
 	# object files are not provided already
 	log "generating object files from IR"
-	
+
+	# TODO: switch to realpath --relative-to?
 	for file in ../IR/**/*.ll; do
-		mkdir -p $( dirname ${file:6} ) 
+		# strip first 6 characters to remove "../IR/"
+		mkdir -p "$( dirname "${file:6}" )" 
 		clang -c "$file" -o "${file:6}".o 
 	done
 
 	log "generating executable from object files"
-	clang $( cat ../LINKER_FLAGS ) -o a.out **/*.o  
+	clang "$( cat ../LINKER_FLAGS )" -o a.out ./**/*.o  
 
 	log "re-building tar archive"
 	cd ..
-	tar -cf prog.tar *
+	tar -cf prog.tar -- *
 
 	log "generating new portable executable"
-	cd $BASE_DIR
-	head -n $(( $TAR_START_POSITION - 1)) $0 | cat > "$OUT_DIR"/new_program.pex
-	cat "$OUT_DIR"/prog.tar >> "$OUT_DIR"/new_program.pex
+	cd "$BASEDIR"
+	head -n $(( "$TAR_START_POSITION" - 1)) "$0" | cat > "$TEMPDIR"/new_program.pex
+	cat "$TEMPDIR"/prog.tar >> "$TEMPDIR"/new_program.pex
 
 	log "substituting existing portable executable"
-	mv "$OUT_DIR"/new_program.pex $0
-	chmod a+x $0
+	mv "$TEMPDIR"/new_program.pex "$0"
+	chmod a+x "$0"
 
-	log "Added binary for tag $ARCH to $0"
+	log "Added binary for tag $BUNDLE_NAME to $0"
 fi
 
 # execute program in subshell, using directory and
 # name of the original call to .pex
 log "executing binary for tag $ARCH"
-( cd $BASE_DIR && exec -a $0 $OUT_DIR/$ARCH/a.out $@ )
+( cd "$BASEDIR" && exec -a "$0" "$TEMPDIR"/"$BUNDLE_NAME"/a.out "$@" )
 
 log "done"
 exit 0	
