3c4eafcbf6d7c39beb8a9ced40b4a826b6287ec9
Linus Hagemann <linus.hagemann@posteo.net>
2020-07-12 11:04:46 +0200

    rename instdir to prefix

--- dev_install.sh
+++ dev_install.sh
@@ -2,12 +2,12 @@
 
 # set to directorz you want the pex suite in
 # this script assumes that the location is part of your PATH
-INSTDIR=/usr/bin
+PREFIX=${PREFIX:-/usr/bin}
 set -e
 DIR=$( dirname $( realpath $0 ) )
 
-cp -sf $DIR/pex.sh $INSTDIR/pex
-cp -sf $DIR/pexmngr.sh $INSTDIR/pexmngr
+cp -sf $DIR/pex.sh $PREFIX/pex
+cp -sf $DIR/pexmngr.sh $PREFIX/pexmngr
 
 cp -sf $DIR/loader.sh /usr/share/pex_loader.sh
 
