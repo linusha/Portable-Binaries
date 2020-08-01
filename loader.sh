cb69ac6c5ba83c63b2a0edcb81ce34801bf994bf
Linus Hagemann <linus.hagemann@posteo.net>
2020-08-01 12:58:40 +0200

    clean up loader

--- loader.sh
+++ loader.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-# Uses env var PEX_USE_TAG to determine the bundle that should be run.
-# If the env var is not set, use architecture triple as default
-# set PEX_VERBOSE for pex logging
-
-if [[ -z $PEX_PROFILE ]]; then
-	PS4='+ $(date "+%s.%N")\011 '
-	exec 3>&2 2>/tmp/pexprofile.$$.log
-	echo "Logging times in /tmp/pexprofile.$$.log"
-	set -x
-fi
+# Uses environment variable PEX_USE_TAG to determine the bundle that should be run.
+# If the variable is not set, use architecture triple as default.
+# Set PEX_VERBOSE for pex logging.
 
 set -e
 shopt -s globstar
 
 function log {
-	# TODO: extract into separate file
 	# Logging function for PEX.
     # Activate output by setting the environment
     # variable PEX_VERBOSE when calling pex.
@@ -30,9 +22,9 @@ function log {
 
 BASEDIR="$PWD"
 TEMPDIR=$( mktemp -d )
-log "Path to temp-folder: "$TEMPDIR""
+log "Path to temp folder: $TEMPDIR"
 
-# Find the number of the line beginning with #__ARCHIVE__BELOW__ with grep.
+# Find the number of the line beginning with #__ARCHIVE__BELOW__.
 # Add one to account for newline.
 TAR_START_POSITION=$(( $( grep -na '^#__ARCHIVE__BELOW__' "$0" | grep -o '^[0-9]*' ) + 1 ))
 
@@ -40,65 +32,62 @@ TAR_START_POSITION=$(( $( grep -na '^#__ARCHIVE__BELOW__' "$0" | grep -o '^[0-9]
 tail -n+"$TAR_START_POSITION" "$0" | tar -x -C "$TEMPDIR"
 
 if [[ -n "$PEX_USE_TAG" ]]; then
-	# if the env variable PEX_USE_TAG is set, use corresponding bundle
+	# If the env variable PEX_USE_TAG is set, use corresponding bundle.
 	BUNDLE_NAME="$PEX_USE_TAG"
 else
-	# determine current cpu architecture
+	# Determine current CPU architecture.
 	BUNDLE_NAME=$( clang -dumpmachine )
 fi
-log "pex tag is set to $BUNDLE_NAME"
+log "Pex tag is set to $BUNDLE_NAME"
 
 BUNDLE_PATH="$TEMPDIR"/"$BUNDLE_NAME"
 
-# program needs to be compiled from IR
+# Program needs to be compiled from IR, because
+# no bundle for current architecture is found.
 if [[ ! -e "$BUNDLE_PATH"/a.out ]]; then
-	# create sub directory for current architecture and change into it
+	# Create sub directory for current architecture and change into it.
 	mkdir -p "$BUNDLE_PATH"
 	cd "$BUNDLE_PATH"
 
-	# object files are not provided already
-	log "generating object files from IR"
+	log "Generating object files from IR"
 
-	# TODO: switch to realpath --relative-to?
+	# TODO: Switch to realpath --relative-to?
 	for file in ../IR/**/*.ll; do
-		# strip first 6 characters to remove "../IR/"
+		# Strip first 6 characters to remove "../IR/".
 		mkdir -p "$( dirname "${file:6}" )" 
 		clang -c "$file" -o "${file:6}".o 
 	done
 
-	log "generating executable from object files"
+	log "Generating executable from object files"
+	# Use saved linker flags for rebuild.
 	clang $( cat ../LINKER_FLAGS ) -o a.out ./**/*.o  
 
-	log "re-building tar archive"
+	log "Re-building tar archive"
 	cd ..
 	tar -cf prog.tar -- *
 
-	log "generating new portable executable"
+	log "Generating new PEX"
 	cd "$BASEDIR"
 	head -n $(( "$TAR_START_POSITION" - 1)) "$0" | cat > "$TEMPDIR"/new_program.pex
 	cat "$TEMPDIR"/prog.tar >> "$TEMPDIR"/new_program.pex
 
-	log "substituting existing portable executable"
+	log "Substituting existing portable executable"
 	mv "$TEMPDIR"/new_program.pex "$0"
+	# TODO: Change permission as needed.
 	chmod a+x "$0"
 
 	log "Added binary for tag $BUNDLE_NAME to $0"
 fi
 
-# execute program in subshell, using directory and
-# name of the original call to .pex
-log "executing binary for tag $ARCH"
+# Execute program in subshell.
+# Set correct working directory and name of the called program (.pex).
+log "Executing binary for tag $BUNDLE_NAME"
 ( cd "$BASEDIR" && exec -a "$0" "$TEMPDIR"/"$BUNDLE_NAME"/a.out "$@" )
 
-log "deleting tmp files"
+log "Deleting tmp files"
 rm -rf "$TEMPDIR"
 
-if [[ -z $PEX_PROFILE ]]; then
-	set +x
-	exec 2>&3 3>&-
-fi
-
-log "done"
+log "Done"
 exit 0	
 
 # After this line the archive is injected.
