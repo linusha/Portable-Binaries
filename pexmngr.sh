c5d58a78c670a57ba0aca41f0fa9dd2767d3d706
Linus Hagemann <linus.hagemann@posteo.net>
2020-08-01 18:51:22 +0200

    clean up -- is needed to fix wildcards for filenames with dashes

--- pexmngr.sh
+++ pexmngr.sh
@@ -1,115 +1,119 @@
 #!/bin/bash
 # This is helper program to interact with pex files in order to:
-# - extract the tar archive,
-# - force recompilation for the current architecture
+# - extract their tar archive,
+# - list their contents (ls or tree style),
+# - remove a bundle,
 # - merge two .pex files for the same program 
-#   (assumes disjunct architecture sets)
-# - list the content of the pex
-# - remove an architecture from a pex
+#   (assumes disjunct bundle names).
 
 set -e
 
-########## HELPERS ##########
-
-function log {
-	echo \[PEX-MNGR\] $1
-}
 function print_usage {
 	echo
-	echo "Possible arguments for portable executable: "
-    echo "    First argument must be the PEX file you want to work with!"\
-         "(except for help)."
+	echo "Possible arguments: "
+    echo "    First argument must be the PEX file you want to work with!"
     echo "    You have to specify exactly one file and operation."
 	echo "    --extract NAME Extract tar archive to directory NAME"
 	echo "    defaults to ./tar"
 	echo "    --merge NAME2 adds the content of pex file NAME2 to PEX"
 	echo "    --ls list the content of PEX"
-    echo "    --tree list the content of PEX with tree program"
-    echo "    --rm [TAG] delete the TAG folder from the PEX"
-    echo "    defaults to current architecture"
+    echo "    --tree list the content of PEX using tree"
+    echo "    --rm [TAG] delete the TAG bundle from the PEX"
+    echo "    defaults to current architecture-triple"
     echo "    --help Display this help message"
 	echo
 }
 
-####### MANAGER LOGIC #######
+function cleanup {
+    # Delete Tempfiles.
+    rm -rf "$TEMPDIR"
+}
 
-# called without arguments or with --help
+# Called without arguments or with --help.
 if [[ -z $1 || $1 == "--help" ]]; then
     print_usage
-    exit 1
+    exit 0
 fi
 
+# Called with only one argument.
 if [[ -z $2 ]]; then
-    log "You have to provide filename and operation."
-    print_usage
+    echo "You have to provide filename and operation."
+    echo "See pexmngr --help."
     exit 1
 fi
 
-PEXFILE=$1
-OPERATION=$2
-ARGUMENT=$3
+PEXFILE="$1"
+OPERATION="$2"
+ARGUMENT="$3"
 
-TAR_START_POSITION=$(( $( grep -na '^#__ARCHIVE__BELOW__' $PEXFILE | grep -o '^[0-9]*' ) + 1 ))
+TAR_START_POSITION=$(( $( grep -na '^#__ARCHIVE__BELOW__' "$PEXFILE" | grep -o '^[0-9]*' ) + 1 ))
 
-if [[ $OPERATION == "--ls" ]]; then
-    tail -n+$TAR_START_POSITION $PEXFILE | tar --list
+
+if [[ "$OPERATION" == "--ls" ]]; then
+    tail -n+"$TAR_START_POSITION" "$PEXFILE" | tar --list
     exit 0
 fi
 
+# Extract tar archive to TEMPDIR.
 TEMPDIR=$(mktemp -d)
+tail -n+"$TAR_START_POSITION" "$PEXFILE" | tar -x -C "$TEMPDIR"
 
-tail -n+$TAR_START_POSITION $PEXFILE | tar -x -C $TEMPDIR
-
-if [[ $OPERATION == "--tree" ]]; then
-    cd $TEMPDIR
+if [[ "$OPERATION" == "--tree" ]]; then
+    cd "$TEMPDIR"
     tree .
-    exit 0
 fi
 
-if [[ $OPERATION == "--extract" ]]; then
-    
-    if [[ -z $ARGUMENT ]]; then
+if [[ "$OPERATION" == "--extract" ]]; then
+    if [[ -z "$ARGUMENT" ]]; then
         ARGUMENT="tar"
     fi
-    mkdir -p $ARGUMENT
-    cp -r $TEMPDIR/* $ARGUMENT
-    log "Extracting to: $ARGUMENT"
-    exit 0
+    mkdir -p "$ARGUMENT"
+    # Archive is already extracted, copy contents outside TEMPDIR.
+    cp -r "$TEMPDIR"/* "$ARGUMENT"
+    echo "Contents in: $ARGUMENT"
 fi
 
-if [[ $OPERATION == "--merge" ]]; then
-    if [[ -z $ARGUMENT ]]; then
-        log "You have to provide a PEX to merge with."
+if [[ "$OPERATION" == "--merge" ]]; then
+    if [[ -z "$ARGUMENT" ]]; then
+        echo "You have to provide a PEX to merge with."
+        cleanup
         exit 1
     fi
-    $0 $ARGUMENT --extract $TEMPDIR
+    # Extract contents from second pex into TEMPDIR.
+    "$0" "$ARGUMENT" --extract "$TEMPDIR"
     BASEDIR=$(pwd)
-    cd $TEMPDIR
-    tar -cf prog.tar *
-    cd $BASEDIR
-    head -n $(( $TAR_START_POSITION - 1)) $PEXFILE | cat > "$TEMPDIR"/new_program.pex
+    cd "$TEMPDIR"
+    # Create new tar archive with merged contents.
+    tar -cf prog.tar -- *
+    cd "$BASEDIR"
+    # Build new .pex file.
+    head -n $(( "$TAR_START_POSITION" - 1)) "$PEXFILE" | cat > "$TEMPDIR"/new_program.pex
     cat "$TEMPDIR"/prog.tar >> "$TEMPDIR"/new_program.pex
-    mv "$TEMPDIR"/new_program.pex $PEXFILE
-    chmod a+x $PEXFILE
-    exit 0
+    # Replace original file with new pex.
+    mv "$TEMPDIR"/new_program.pex "$PEXFILE"
+    # TODO: Change permission as needed.
+	# Good solution would be to save and reapply initial permissions.
+    chmod a+x "$PEXFILE"
 fi
 
-if [[ $OPERATION == "--rm" ]]; then
-    if [[ -z $ARGUMENT ]]; then
+if [[ "$OPERATION" == "--rm" ]]; then
+    if [[ -z "$ARGUMENT" ]]; then
+    # Use current architecture-triple as default.
         ARGUMENT=$( clang -dumpmachine )
     fi
     BASEDIR=$(pwd)
-    cd $TEMPDIR
-    rm -rf $ARGUMENT
-    tar -cf prog.tar *
-    cd $BASEDIR
-    head -n $(( $TAR_START_POSITION - 1)) $PEXFILE | cat > "$TEMPDIR"/new_program.pex
+    cd "$TEMPDIR"
+    # Remove specified bundle.
+    rm -rf "$ARGUMENT"
+    # Build new .pex file.
+    tar -cf prog.tar -- *
+    cd "$BASEDIR"
+    head -n $(( "$TAR_START_POSITION" - 1)) "$PEXFILE" | cat > "$TEMPDIR"/new_program.pex
     cat "$TEMPDIR"/prog.tar >> "$TEMPDIR"/new_program.pex
-    mv "$TEMPDIR"/new_program.pex $PEXFILE
-    chmod a+x $PEXFILE
-    exit 0
+    mv "$TEMPDIR"/new_program.pex "$PEXFILE"
+    # TODO: Change permission as needed.
+    chmod a+x "$PEXFILE"
 fi
 
-rm -rf "$TEMPDIR"
-
-exit 1
+cleanup
+exit 0
